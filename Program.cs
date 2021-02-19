dfa4782f3e36e85b776cf91999261e16d99cc7a2
Megan Nyland <Megan Nyland@DESKTOP-2LAHT55>
2021-02-18 23:55:57 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Deliverable_3_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name?:  ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello " + input + "!");
+        }
+    }
+}
